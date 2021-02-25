cfacc1009967b9c43e2e53d7f2496ccf95dfbf24
keithconger <keith.conger@blackboxembedded.com>
2021-02-25 16:49:38 -0700

    App Launcher

---
+++ app/src/main/java/com/blackboxembedded/WunderLINQ/AppListActivity.java
@@ -0,0 +1,226 @@
+/*
+WunderLINQ Client Application
+Copyright (C) 2020  Keith Conger, Black Box Embedded, LLC
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+package com.blackboxembedded.WunderLINQ;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppListActivity extends AppCompatActivity {
+
+    public final static String TAG = "AppList";
+
+    private ListView appList;
+
+    PackageManager packageManager;
+    private SharedPreferences sharedPrefs;
+
+    private static List<AppInfo> apps;
+    private int lastPosition = 0;
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_list);
+
+        sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        AppUtils.adjustDisplayScale(this, getResources().getConfiguration());
+        // Keep screen on
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        appList = findViewById(R.id.lv_apps);
+        appList.setOnTouchListener(new OnSwipeTouchListener(this) {
+            @Override
+            public void onSwipeRight() {
+                goBack();
+            }
+        });
+
+        String orientation = sharedPrefs.getString("prefOrientation", "0");
+        if (!orientation.equals("0")){
+            if(orientation.equals("1")){
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            } else if (orientation.equals("2")){
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
+            } else {
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            }
+        } else {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER);
+        }
+
+        showActionBar();
+
+        loadApps();
+
+        updateList();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        int highlightColor = androidx.preference.PreferenceManager.getDefaultSharedPreferences(this).getInt("prefHighlightColor", R.color.colorAccent);
+        appList.setSelector(new ColorDrawable(highlightColor));
+
+        updateList();
+    }
+
+    private void showActionBar(){
+        LayoutInflater inflator = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflator.inflate(R.layout.actionbar_nav, null);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(false);
+        actionBar.setDisplayShowHomeEnabled(false);
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setCustomView(v);
+
+        TextView navbarTitle = findViewById(R.id.action_title);
+        navbarTitle.setText(R.string.applist_title);
+
+        ImageButton backButton = findViewById(R.id.action_back);
+        backButton.setOnClickListener(mClickListener);
+
+        ImageButton forwardButton = findViewById(R.id.action_forward);
+        forwardButton.setVisibility(View.INVISIBLE);
+    }
+
+    private void goBack(){
+        Intent backIntent = new Intent(AppListActivity.this, com.blackboxembedded.WunderLINQ.TaskList.TaskActivity.class);
+        startActivity(backIntent);
+    }
+
+    private View.OnClickListener mClickListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            switch(v.getId()) {
+                case R.id.action_back:
+                    // Go back
+                    goBack();
+                    break;
+            }
+        }
+    };
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_DPAD_LEFT:
+                goBack();
+                return true;
+            case KeyEvent.KEYCODE_DPAD_DOWN:
+                if ((appList.getSelectedItemPosition() == (apps.size() - 1)) && lastPosition == (apps.size() - 1) ){
+                    appList.setSelection(0);
+                }
+                lastPosition = appList.getSelectedItemPosition();
+                return true;
+            case KeyEvent.KEYCODE_DPAD_UP:
+                if (appList.getSelectedItemPosition() == 0 && lastPosition == 0){
+                    appList.setSelection(apps.size() - 1);
+                }
+                lastPosition = appList.getSelectedItemPosition();
+                return true;
+            default:
+                return super.onKeyUp(keyCode, event);
+        }
+    }
+
+    public void updateList(){
+        AppListView adapter = new AppListView(this, apps);
+        appList = findViewById(R.id.lv_apps);
+        appList.setAdapter(adapter);
+        appList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                lastPosition = position;
+                Intent intent = packageManager.getLaunchIntentForPackage(apps.get(position).name.toString());
+                AppListActivity.this.startActivity(intent);
+            }
+        });
+
+    }
+
+    private void loadApps() {
+        try {
+            if (packageManager == null)
+                packageManager = getPackageManager();
+            if (apps == null) {
+                apps = new ArrayList<AppInfo>();
+
+                Intent i = new Intent(Intent.ACTION_MAIN, null);
+                i.addCategory(Intent.CATEGORY_LAUNCHER);
+
+                List<ResolveInfo> availableApps = packageManager.queryIntentActivities(i, 0);
+                for (ResolveInfo ri : availableApps) {
+                    AppInfo appinfo = new AppInfo();
+                    appinfo.label = ri.loadLabel(packageManager);
+                    appinfo.name = ri.activityInfo.packageName;
+                    appinfo.icon = ri.activityInfo.loadIcon(packageManager);
+                    Log.d(TAG, "name: " + ri.activityInfo.packageName);
+                    apps.add(appinfo);
+                }
+            }
+
+        } catch (Exception ex) {
+            Log.e(TAG, ex.getMessage().toString() + " loadApps");
+        }
+    }
+}
---
+++ app/src/main/java/com/blackboxembedded/WunderLINQ/AppListView.java
@@ -0,0 +1,57 @@
+/*
+WunderLINQ Client Application
+Copyright (C) 2020  Keith Conger, Black Box Embedded, LLC
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+package com.blackboxembedded.WunderLINQ;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class AppListView extends ArrayAdapter<AppInfo> {
+
+    private final Activity context;
+    private final List<AppInfo> apps;
+
+    public AppListView(Activity context,
+                       List<AppInfo> apps) {
+        super(context, R.layout.item_contact, apps);
+        this.context = context;
+        this.apps = apps;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View rowView= inflater.inflate(R.layout.item_contact, null, true);
+        TextView txtTitle = rowView.findViewById(R.id.tv_label);
+        ImageView imageView = rowView.findViewById(R.id.iv_icon);
+
+        txtTitle.setText(apps.get(position).label);
+        if (apps.get(position).icon != null) {
+            imageView.setImageDrawable(apps.get(position).icon);
+            imageView.setImageTintMode(null);
+        }
+
+        return rowView;
+    }
+}
